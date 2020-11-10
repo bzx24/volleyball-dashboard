9af6ba67ac7d4c90f133ad835b65a4a3355b9c56
Brian Xu <brianxu16@gmail.com>
2020-11-10 09:45:53 -0600

    creating attack eff metric

--- data.R
+++ data.R
@@ -32,3 +32,15 @@ if(length(files) > 1) {
   }
 }
 remove(hold, x, i, files)
+
+#create attack efficiency metric
+attacks <- data_6v6 %>%
+  filter(skill == "Attack") %>%
+  group_by(player_name) %>%
+  mutate(attack_kill = sum(evaluation == "Winning attack"),
+         attack_error = sum(evaluation %in% c("Blocked", "Error"))) %>%
+  group_by(player_name,
+           attack_kill,
+           attack_error) %>%
+  summarise(attack_att = n()) %>%
+  mutate(attack_eff = (attack_kill - attack_error) / attack_att)
